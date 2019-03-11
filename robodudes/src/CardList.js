554e1e103ca9d5d90a4ba5434bc44954a8314bd5
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2019-03-10 18:56:53 -0600

    finished cardlist

--- robodudes/src/CardList.js
+++ robodudes/src/CardList.js
@@ -2,12 +2,19 @@ import React from "react";
 
 import Card from "./Card.js";
 const CardList = ({robots})=>{
-  const cardComponent = robots.map((users, i )=>{
-  return  <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-})
+
 return (
   <div>
-  {cardComponent}
+  {
+  robots.map((users, i )=>{
+  return ( <Card
+   key={i}
+   id={robots[i].id}
+   name={robots[i].name}
+   email={robots[i].email} />
+   )
+ })
+}
 </div>
 )
 
